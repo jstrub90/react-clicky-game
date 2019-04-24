f7a5b4e436003cee0812d4100e3748a00861b663
J.Strub <jstrub90@gmail.com>
2019-04-24 18:23:38 -0400

    Updates

---
+++ precache-manifest.53d2b9b8b24283575ad33d7626efb212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc9b231c5ef87a14d58ec8bef7f8853c",
+    "url": "/react-clicky-game/static/media/ocean-wallpaper-hd-5.cc9b231c.jpg"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  },
+  {
+    "revision": "e05db7781bfb373d8782",
+    "url": "/react-clicky-game/static/js/main.21f9063c.chunk.js"
+  },
+  {
+    "revision": "1ad258c8ad11d64bafc3",
+    "url": "/react-clicky-game/static/js/2.e3afa17f.chunk.js"
+  },
+  {
+    "revision": "e05db7781bfb373d8782",
+    "url": "/react-clicky-game/static/css/main.6ff88f1b.chunk.css"
+  },
+  {
+    "revision": "77924da048ecf630a4b083fd98f4619a",
+    "url": "/react-clicky-game/index.html"
+  }
+];
